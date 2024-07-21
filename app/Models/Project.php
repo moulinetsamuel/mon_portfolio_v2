583379b7016c3b6f94a886ad6a06c986f425c0c4
moulinetsamuel <samuel.moulinet@oclock.school>
2024-07-21 19:07:34 +0200

    feat: Add models for CV, Contact, Project, ProjectImage, and Stack

---
+++ app/Models/Project.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'description', 'url', 'github_url'];
+
+    public function images()
+    {
+        return $this->hasMany(ProjectImage::class);
+    }
+
+    public function stacks()
+    {
+        return $this->belongsToMany(Stack::class);
+    }
+}
