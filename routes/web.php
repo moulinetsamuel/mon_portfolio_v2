cc92d967e2b9a30e1a5c94d814fecbda03d0a3ee
moulinetsamuel <samuel.moulinet@oclock.school>
2024-07-22 11:16:00 +0200

    Add ProjectController for managing projects

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('projects', ProjectController::class);
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
